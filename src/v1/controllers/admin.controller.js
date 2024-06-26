b87b93c77030b542c97f611ab2c5aa4e939aa671
faisalcreativeheads <100824711+faisalcreativeheads@users.noreply.github.com>
2024-06-26 16:23:00 +0100

    refactor: fix

--- src/v1/controllers/admin.controller.js
+++ src/v1/controllers/admin.controller.js
@@ -221,20 +221,20 @@ const createAdmin = async (req, res, next) => {
   try {
     const { email, password } = req.body
 
-    function isValidEmail(value) {
-      const re =
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      return re.test(String(value).toLowerCase())
-    }
+    // function isValidEmail(value) {
+    //   const re =
+    //     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    //   return re.test(String(value).toLowerCase())
+    // }
 
-    if (!email || !password || password.length < 6 || !isValidEmail(email)) {
-      return apiResponse.validationErrorWithData(
-        res,
-        'Beklager, det oppstod en valideringsfeil.',
-        'Validation Error',
-        'Invalid Data'
-      )
-    }
+    // if (!email || !password || password.length < 6 || !isValidEmail(email)) {
+    //   return apiResponse.validationErrorWithData(
+    //     res,
+    //     'Beklager, det oppstod en valideringsfeil.',
+    //     'Validation Error',
+    //     'Invalid Data'
+    //   )
+    // }
 
     req.body.image = req?.file?.location || ''
     const { ...itemDetails } = req.body
