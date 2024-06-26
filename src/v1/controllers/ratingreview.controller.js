b2def13507764e69f8ee298d3d0aea30a6164047
faisalcreativeheads <100824711+faisalcreativeheads@users.noreply.github.com>
2024-06-26 18:40:51 +0100

    refactor: fix rating

--- src/v1/controllers/ratingreview.controller.js
+++ src/v1/controllers/ratingreview.controller.js
@@ -15,13 +15,29 @@ const createRating = async (req, res, next) => {
       itemName: 'Rating Review',
     })
     if (status) {
-      const ratings = await RatingReview.find({ user_id: user_id }).exec()
-      let totalRating = 0
-      ratings.forEach((r) => {
-        totalRating += r.rating
-      })
-      const avgRating = totalRating / ratings.length
-      const totalRatingOutOf5 = avgRating / 5
+      // const ratings = await RatingReview.find({ user_id: user_id }).exec()
+      // let totalRating = 0
+      // ratings.forEach((r) => {
+      //   totalRating += r.rating
+      // })
+      // const avgRating = totalRating / ratings.length
+      // const totalRatingOutOf5 = avgRating / 5
+
+      const ratings5 = await RatingReview.find({ user_id: user_id, rating: 5 }).count()
+      const ratings4 = await RatingReview.find({ user_id: user_id, rating: 4 }).count()
+      const ratings3 = await RatingReview.find({ user_id: user_id, rating: 3 }).count()
+      const ratings2 = await RatingReview.find({ user_id: user_id, rating: 2 }).count()
+      const ratings1 = await RatingReview.find({ user_id: user_id, rating: 1 }).count()
+      const makeRate5 = ratings5 * 5
+      const makeRate4 = ratings4 * 4
+      const makeRate3 = ratings3 * 3
+      const makeRate2 = ratings2 * 2
+      const makeRate1 = ratings1 * 1
+
+      const totalRating = makeRate5 + makeRate4 + makeRate3 + makeRate2 + makeRate1
+      console.log({ ratings5, ratings4, ratings3, ratings2, ratings1 })
+
+      const ratinOutof5 = totalRating / 5
 
       await updateItemReturnData({
         Model: UserModal,
@@ -29,7 +45,7 @@ const createRating = async (req, res, next) => {
           _id: user_id,
         },
         updateobject: {
-          rating: totalRatingOutOf5,
+          rating: ratinOutof5.toFixed(1) >= 5 ? 5 : ratinOutof5.toFixed(1),
         },
         req,
         res,
